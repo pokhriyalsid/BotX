301fb977a25bf84efa0656179b6953c32dfe6c9a
pokhriyalsid <55281492+pokhriyalsid@users.noreply.github.com>
2020-04-17 12:01:09 +0100

    updated func2.py

--- function2/func2.py
+++ function2/func2.py
@@ -13,7 +13,20 @@ import os
 sys.path.append(os.path.dirname(os.getcwd())) ## Modifying sys.path in order to use CommonFunc package
 
 from CommonFunc import CommonFunc # from CommonFunc package importing CommonFunc module
+Username = 'test'
+Pwd = 'test'
 
-cmds = CommonFunc.commandlist(os.path.dirname(os.getcwd()) + '\\Jsonfiles' + '\\func2commands.json', 'Juniper') ## commandlist function excepts the path of the file
+#cmds = CommonFunc.commandlist(os.path.dirname(os.getcwd()) + '\\Jsonfiles' + '\\func2commands.json', 'Juniper') ## commandlist function excepts the path of the file
 
-print(cmds)
+Ciscodevicelist, juniperdevicelist = CommonFunc.devicelist(os.path.dirname(os.getcwd()) + '\\Jsonfiles' + '\\DeviceList.json')
+## Ciscodevicelist and juniperdevice list gives both name and ip of the device
+Ciscocommandlist = CommonFunc.commandlist(os.path.dirname(os.getcwd()) + '\\Jsonfiles' + '\\func2commands.json', 'Cisco')
+Junipercommandlist = CommonFunc.commandlist(os.path.dirname(os.getcwd()) + '\\Jsonfiles' + '\\func2commands.json', 'Juniper')
+
+# Here later i will add code to update user that which commands will be run on which devices and also if they want to update the command list or devicelist then they will be redirected to a different function.
+cisconetmikodictobj = []  ## This list will have the parameters in the format we pass to netmiko ConnectHandler
+if len(Ciscodevicelist) > 0:
+    for items in Ciscodevicelist:
+        cisconetmikodictobj.append({'device_type':'cisco_ios' , 'host':items['IP'], 'username':Username, 'Password':Pwd, 'secret' : 'test'})
+if len(Ciscodevicelist) == 0:
+    print("There are no Cisco device mentioned in the file to run commands on")
