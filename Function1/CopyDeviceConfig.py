93e29f629e8dcfcdc9c423b8e2e4c89cc023f5fb
pokhriyalsid <55281492+pokhriyalsid@users.noreply.github.com>
2020-04-28 10:12:45 +0100

    Now Running config are saved with Hostnames instead of IP

--- Function1/CopyDeviceConfig.py
+++ Function1/CopyDeviceConfig.py
@@ -17,7 +17,7 @@ Pwd = 'test'
 x = time.asctime()    # x is a string object returned
 timestring = x.split()
 
-def devicelogin(devicelist):
+def devicelogin(devicelist, DeviceN):
     try:
         netconnect = netmiko.ConnectHandler(**devicelist)
     except netmiko.ssh_exception.NetmikoTimeoutException:
@@ -28,12 +28,14 @@ def devicelogin(devicelist):
         netconnect.send_command("terminal length 0")
         runconfig = netconnect.send_command("show running-config")
         try :
-            FolderName = os.path.dirname(os.getcwd()) + '\\' + 'ScriptOutput' + '\\' + 'RunningConfig' + '\\'+ devicelist['host']
+            FolderName = os.path.dirname(os.getcwd()) + '\\' + 'ScriptOutput' + '\\' + 'RunningConfig' + '\\'+ DeviceN
+
+            #devicelist['host']
             os.mkdir(FolderName) ## This will create a folder with name of the Router
             ## This code is under try to catch exception if folder already exists
         except FileExistsError:
             pass
-        pathrun = FolderName + "\\" + devicelist['host'] + "_" + timestring[2] + "_" + timestring[1] + "_" +  timestring[3].split(':')[0] + "Hour" + "_" + "run.txt"
+        pathrun = FolderName + "\\" + DeviceN + "_" + timestring[2] + "_" + timestring[1] + "_" +  timestring[3].split(':')[0] + "Hour" + "_" + "run.txt"
         with open(pathrun, 'w+') as outputfile:
             outputfile.write(runconfig)
 
@@ -47,7 +49,17 @@ else:
     n = 1
     ciscothreadlist = []
     for items in cisconetmikoobj:
-        ciscothreadlist.append(threading.Thread(target=devicelogin, name = 'Thread'+ str(n), args =[items]))
+        ciscothreadlist.append(threading.Thread(target=devicelogin, name = 'Thread'+ str(n), args =[items, Ciscodevicelist[n-1]['Name']]))
+        n = n + 1
 
+    n = 0
     for threads in ciscothreadlist:
+        if (n+1)%5 == 0:            ## This will make sure if more than 5 devices are there is the device list
+            threads.join()      ## then after the 5th device there is some pause in the code
+            while True:
+                no = 1
+                if ciscothreadlist[n].is_alive():
+                    print("")
+
         threads.start()
+        n = n + 1
