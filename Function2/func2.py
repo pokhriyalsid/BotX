4c968893f49e24c3d0ce6d84459881d0c4726b71
pokhriyalsid <55281492+pokhriyalsid@users.noreply.github.com>
2020-05-07 11:09:48 +0100

    Minor changes in func2.py

--- Function2/func2.py
+++ Function2/func2.py
@@ -1,10 +1,9 @@
-#The Function performs below functions:
-# Login to all devices and checks which Vlan, ip address subnets are behind them
-
-# It will take the list of devices from the file input device which is created using a specific format
-# This function should also ask twice if you want to run the command.
-# It should also show the list of commands before executing
-# It should only run show commands
+# The module has below functions
+# Mainfunc2() : This is the main function which calls other function based on user input
+# devicelogin() : This function is called inside Mainfunc2_1 and used to create threads
+# Mainfunc2_1() : This function initiates the threads
+# deviceloginCustom() : this would be used in customcmds function and used to create threads
+# customcmds() : Function to run custom commands to mentioned list of devices
 
 import netmiko
 import sys
@@ -65,7 +64,7 @@ def devicelogin(list, cmdlist):
         print('************************', file = file)
         print('\n'*3, file = file)
         file.close()
-# Here later i will add code to update user that which commands will be run on which devices and also if they want to update the command list or devicelist then they will be redirected to a different function.
+
 def Mainfunc2_1():
     cisconetmikoobj = []  ## This list will have the parameters in the format we pass to netmiko ConnectHandler
     if len(Ciscodevicelist) > 0:
